1f44bec60f13182803aa5f9e31794626e303f90d
NaafiulRazzaqw <naafiulrazzaq@gmail.com>
2025-01-28 10:03:24 +0700

    Membuat Resource Hero Images dan FAQ

---
+++ app/Filament/Resources/FaqsResource/Pages/CreateFaqs.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Filament\Resources\FaqsResource\Pages;
+
+use App\Filament\Resources\FaqsResource;
+use Filament\Actions;
+use Filament\Pages\Actions\Action;
+use Filament\Resources\Pages\CreateRecord;
+
+
+class CreateFaqs extends CreateRecord
+{
+    protected static string $resource = FaqsResource::class;
+
+    public function getHeading(): string
+    {
+        return __('Create FAQ');
+    }
+
+    
+
+
+}
