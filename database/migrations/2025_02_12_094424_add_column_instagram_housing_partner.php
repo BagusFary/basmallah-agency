76e7d816972a044d2f52c6580c98a2d940ed31e8
Affansyah Hanan W <afanhanan10@gmail.com>
2025-02-12 18:03:24 +0700

    Change Instagram Migration Stage Down

--- database/migrations/2025_02_12_094424_add_column_instagram_housing_partner.php
+++ database/migrations/2025_02_12_094424_add_column_instagram_housing_partner.php
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('housing_partners', function (Blueprint $table) {
-            $table->string('instagram')->nullable()->after('email');
+            $table->dropColumn('instagram');
         });
     }
 };
